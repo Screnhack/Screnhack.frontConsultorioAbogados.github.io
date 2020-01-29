c554ac37ac67db749812983f60a5681230ee3589
Screnhack <camelo_38@hotmail.com>
2020-01-29 08:38:41 -0500

    Sacar TCRM

--- src/app/feature/home/home.component.ts
+++ src/app/feature/home/home.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { TcrmService } from 'src/app/shared/tcrm/service/tcrm.service';
 
 @Component({
   selector: 'app-home',
@@ -7,21 +6,11 @@ import { TcrmService } from 'src/app/shared/tcrm/service/tcrm.service';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  tcrm: string ;
 
   constructor(
-    private tcrmService: TcrmService
   ) { }
 
   ngOnInit() {
-
-    this.consultarTcrm();
-  }
-
-  public consultarTcrm(){
-    this.tcrmService.getConsumoTcrm().subscribe(
-      response => {this.tcrm = response}
-    )
   }
 
 
